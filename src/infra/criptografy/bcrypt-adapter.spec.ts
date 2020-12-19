15e66069d3a80dfd5ca9bec5a459394fc6a57326
Natalia Silva <nataliadasilva.dev@gmail.com>
2020-12-19 18:40:22 -0300

    test: ensure BcryptAdapter throws when bcrypt throws

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -23,6 +23,15 @@ describe('Bcrypt Adapter', () => {
   test('Should return a hash on success', async () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
-    await expect(hash).toBe('hash')
+    expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
